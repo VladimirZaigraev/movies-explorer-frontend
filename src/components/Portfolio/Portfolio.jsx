2e6c8cb2f1ea87417812a6e8beb9c01dfe7514cb
Vladimir Zigraev <v.a.zaigraev@mail.ru>
2022-05-04 17:23:23 +0300

    create portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,21 @@
 //Portfolio — компонент со ссылками на другие проекты.
 import React from 'react'
-import './index.sass'
+import { Link } from 'react-router-dom'
+import arrow from '../../images/arrow.svg'
+import './Portfolio.sass'
 
 export const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section className="portfolio">
+      <div className="container">
+        <h3 className="portfolio__title">Портфолио</h3>
+        <ul className="portfolio__list list">
+          <li className="portfolio__item item">
+            <Link className="portfolio__link portfolio-link" to='#'>
+              <p className="portfolio-link__text">Статичный сайт</p> <img className="portfolio-link__arrow" src={arrow} alt="arrow" /></Link>
+          </li>
+        </ul>
+      </div>
+    </section>
   )
 }
