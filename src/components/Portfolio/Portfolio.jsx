4926f764ec2c3b1d05c33ab65a2fab150036d5e2
Vladimir Zigraev <v.a.zaigraev@mail.ru>
2022-05-05 14:33:29 +0300

    update portfolio, aboutme: adaptiv style

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,16 +4,42 @@ import { Link } from 'react-router-dom'
 import arrow from '../../images/arrow.svg'
 import './Portfolio.sass'
 
+const portfolioSite = [
+  {
+    id: 1,
+    title: "Статичный сайт",
+    link: "https://vladimirzaigraev.github.io/how-to-learn/",
+  },
+  {
+    id: 2,
+    title: "Адаптивный сайт",
+    link: "https://vladimirzaigraev.github.io/russian-travel/",
+  },
+  {
+    id: 3,
+    title: "Одностраничное приложение",
+    link: "https://zaigraev.nomoredomains"
+  }
+]
+
 export const Portfolio = () => {
   return (
     <section className="portfolio">
       <div className="container">
         <h3 className="portfolio__title">Портфолио</h3>
         <ul className="portfolio__list list">
-          <li className="portfolio__item item">
-            <Link className="portfolio__link portfolio-link" to='#'>
-              <p className="portfolio-link__text">Статичный сайт</p> <img className="portfolio-link__arrow" src={arrow} alt="arrow" /></Link>
-          </li>
+          {
+            portfolioSite.map((elem) => {
+              return (
+                <li className="portfolio__item item" key={elem.id}>
+                  <Link className="portfolio__link portfolio-link" to={elem.link}>
+                    <p className="portfolio-link__text">{elem.title}</p>
+                    <img className="portfolio-link__arrow" src={arrow} alt="arrow" />
+                  </Link>
+                </li>
+              )
+            })
+          }
         </ul>
       </div>
     </section>
